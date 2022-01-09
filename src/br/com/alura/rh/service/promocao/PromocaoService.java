1f40164416dd3aa0f68503907ddd471d243ce4d2
Felipe Ferreira <fellipe_fb@hotmail.com>
2022-01-09 18:10:25 -0300

    Treinamento SOLID - ALURA Liskov Substitution Principle

--- src/br/com/alura/rh/service/promocao/PromocaoService.java
+++ src/br/com/alura/rh/service/promocao/PromocaoService.java
@@ -1,15 +1,21 @@
 package br.com.alura.rh.service.promocao;
 
 import br.com.alura.rh.model.Funcionario;
+import br.com.alura.rh.ValidacaoException;
+import br.com.alura.rh.model.Cargo;
 
 public class PromocaoService {
 	
 	public void promover(Funcionario funcionario, boolean metaBatida) {
-		if(funcionario.getCargo() == Cargo.GERENTE) {
-			
+		Cargo cargoAtual = funcionario.getDadosPessoais().getCargo();
+		if(cargoAtual == Cargo.GERENTE) {
+			throw new ValidacaoException("Gerentes não podem ser promovidos!");
+		} else if(metaBatida) {
+			Cargo novoCargo = cargoAtual.getProximoCargo();
+			funcionario.promover(novoCargo);
+		} else {
+			throw new ValidacaoException("Funcionário não bateu a meta!");
 		}
-		
-		
 	}
 
 }
--- src/br/com/alura/rh/service/reajuste/ReajusteService.java
+++ src/br/com/alura/rh/service/reajuste/ReajusteService.java
@@ -17,8 +17,7 @@ public class ReajusteService {
 	public void reajustarSalarioDoFuncionario(Funcionario funcionario,BigDecimal aumento) {
 		
 		validacoes.forEach(v -> v.validar(funcionario, aumento));
-		
-		BigDecimal salarioReajustado = funcionario.getSalario().add(aumento);
+		BigDecimal salarioReajustado = funcionario.getDadosPessoais().getSalario().add(aumento);
 		funcionario.atualizarSalario(salarioReajustado);
 	}
 
